f9421aab12bc6914432c9e58b511da7e27427f2e
Domenico Sacco <dislif77@virgilio.it>
2023-02-05 15:08:15 +0100

    added class Rappresentable

--- app/models.py
+++ app/models.py
@@ -1,1 +1,9 @@
 from app import db 
+from sqlalchemy.inspection import inspect
+
+class Rappresentable:
+
+    def __repr__(self) -> str:
+        attrs = [getattr(self, attr) for attr in inspect(self.__class__).c.keys()]
+        repr = '<{}:' + ''.join(['\n  {},' for attr in attrs][:-1]) + '\n>'
+        return repr.format(self.__class__.__name__, *attrs)
