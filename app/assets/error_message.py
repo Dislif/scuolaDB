97188a3df1f1607da735c1b169a2af5387033431
Domenico Sacco <dislif77@virgilio.it>
2023-02-26 13:19:53 +0100

    Added assets form for errors and regexes

---
+++ app/assets/error_message.py
@@ -0,0 +1,17 @@
+
+input_required_error = 'Il campo è obbligatorio'
+
+tel_error = 'Il campo deve contenere solo numeri. In notazione 3-3-4 o 10 numeri senza spazi'
+
+str_error = 'Il campo non deve contenere numeri o caratteri speciali'
+
+via_error = str_error + ' e deve iniziare con uno tra i seguenti termini: Via, Viale, Corso, Piazza, Piazzale'
+
+civico_error = 'Il campo deve contenere un numero maggiore di 0'
+
+email_error = 'Il campo deve contenere un email'
+
+password_error = 'La password non corrisponde ai criteri minimi:\n\t. Almeno una lettera maiuscola\n\t. Almeno una lettera minuscola\n\t. Almeno un numero\n\t. Almeno un carattere speciale\n\t. Minimo 8 caratteri'
+
+confirm_pass_error = 'Il campo deve essere uguale alla password'
+
