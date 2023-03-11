1fb707a67e8c7e4a693882cab3d46f6cacf058c5
Domenico Sacco <dislif77@virgilio.it>
2023-03-11 16:25:08 +0100

    updated register method to inserts the entries in the tables

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, db
 from app.forms import RegisterForm, LoginForm, ProvaForm
 from flask import render_template, redirect, url_for, session
+from app.models import Utente, Anagrafica
 
 @app.route('/')
 def index():
@@ -24,5 +25,15 @@ def prova():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
+        user = Utente(email=form.email.data)
+        user.set_password(form.password.data)
+        params = form.data
+        #list(map(lambda p : params.pop(p), ['submit','csrf_token','conf_pass']))
+        for field in ['submit','csrf_token','conf_pass','password','email']:
+            params.pop(field)
+        anagraphics = Anagrafica(**params, utente=user)
+        for entry in [user, anagraphics]:
+            db.session.add(entry)
+        db.session.commit()
         return redirect(url_for('index'))
     return render_template('registration.html', form=form)
